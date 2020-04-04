c59751166f628f6378abcde92a42d10ee23b17d0
yas777 <32452559+yas777@users.noreply.github.com>
2020-04-04 20:48:06 +0530

    Q2 mainscript updated

--- assignmentReconstruction/code/myMainscript2.m
+++ assignmentReconstruction/code/myMainscript2.m
@@ -1,22 +1,25 @@
 %% Reading image
 
-I = imread("../data/ChestCT.png");
+I = imread("../data/ChestPhantom.png");
+[M, N] = size(I);
 
 %% CT_imageing (2.1)
-th1 = 0;
-th2 = 179;
-diff = 1;
-
-th = th1:diff:th2;
-R = radon(I, th);
-A = Back_projection(R);
-imshow(A);
+drho = 5;
+dtheta = 1;
+A = Radon_matrix(drho, dtheta, M, N);
 
 %% (2.2)
 
-R = radon(A, th);
+R = A*I(:);
 R_noisy = imnoise(R, 'gaussian', 0, 0.02);
 
 %% (2.3)
 
-A_noisy = iradon(R_noisy, th, 'linear', 'Ram-Lak');
+I_noisy = iradon(R_noisy, th, 'linear', 'Ram-Lak');
+
+%% (2.4)
+
+lambda = 0.1;
+I4 = inv((A'*A + lambda)A'*R_noisy
+
+%% (2.5)
