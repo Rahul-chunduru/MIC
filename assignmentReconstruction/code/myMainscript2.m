bdad1dc12d6e077170adf6a3881757c4d5652e36
Rahul-chunduru <chundururahul@gmail.com>
2020-04-05 01:06:57 +0530

    some patch up for q2

--- assignmentReconstruction/code/myMainscript2.m
+++ assignmentReconstruction/code/myMainscript2.m
@@ -1,27 +1,37 @@
 %% Reading image
 
-I = imread("../data/ChestPhantom.png");
+I = im2double(imread('../data/ChestPhantom.png'));
+theta = 0:179 ; 
 [M, N] = size(I);
 
 %% CT_imageing (2.1)
-drho = 5;
+drho = 2;
 dtheta = 1;
 A = Radon_matrix(drho, dtheta, M, N);
 
 %% (2.2)
 
-R = A*I(:);
+R = A * I(:);
 R_noisy = imnoise(R, 'gaussian', 0, 0.02);
 
 %% (2.3)
 
-I_noisy = iradon(R_noisy, th, 'linear', 'Ram-Lak');
+Rt_I_noisy = reshape(R_noisy, [], 180) ; 
+I_noisy = iradon(Rt_I_noisy, theta, 'linear', 'Ram-Lak', 128);
+imshow(I_noisy) ; 
 
 %% (2.4)
 
 lambda = 0.1;
-I4 = inv((A'*A + lambda)A'*R_noisy
+I4 = (A'*A + lambda)\A' * R_noisy ; 
 
 %% (2.5)
+
+I_rec = zeros(size(I)); 
+
+
+
+
+
 
 
