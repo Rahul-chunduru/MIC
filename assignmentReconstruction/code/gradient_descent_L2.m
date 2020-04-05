9b5ce7efac0a11a13b9c21fcee85b9d6a0576916
yas777 <32452559+yas777@users.noreply.github.com>
2020-04-05 21:58:10 +0530

    Updated gradient_DescentL2

--- assignmentReconstruction/code/gradient_descent_L2.m
+++ assignmentReconstruction/code/gradient_descent_L2.m
@@ -1,13 +1,17 @@
-function [X] = gradient_descent_L2(A, b, alp, eps, M, s)
-    rng(s);
-    X = randn(M, 1);
-    nl = loss(A, X, b);
-    pl = 0;
+function [X] = gradient_descent_L2(A, I_noisy, b, alp, eps)
+    X = I_noisy(:);
+    pl = loss(A, X, b, alp);
     c = 0;
-    while (pl - nl > 0.0001 || c == 0)
-        X = X - eps*((alp/2)*X + 2*(A')*(A*X - b));
-        pl = nl;
-        nl = loss(A, X, b);
+    while (c < 50)
+        X1 = X - eps*((alp/2)*X + 2*(A')*(A*X - b));
+        nl = loss(A ,X1, b, alp);
+        if nl < pl
+            pl = nl;
+            eps = eps*1.1;
+            X = X1;
+        else
+            eps = eps/2;
+        end
         c = c + 1;
     end
 end
